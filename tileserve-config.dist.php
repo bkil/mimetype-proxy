76941052099e09e2e6d546e6effa46de5387091c
bkil <b.K.il.h.u+tigbuh@gmail.com>
2021-05-13 19:12:46 +0200

    tileserve-config.dist.php: add

---
+++ tileserve-config.dist.php
@@ -0,0 +1,6 @@
+<?php
+
+$allowedUrl = "~^https?://tile[0-9]\.example\.com/maps/[a-zA-Z0-9_]+/[a-zA-Z0-9_]+/[a-zA-Z0-9_]+/[0-9]+/[0-9]+/[0-9]+$~";
+$allowedUserAgent = "~^example/[0-9]~";
+$sentUserAgent = "mimetype/0.1";
+$adminEmail = "example@example.com";
